bc4bb82ddeeb414ba406442550db95d0f3a96b72
Rajath R Pai <44657512+rajatpai@users.noreply.github.com>
2022-05-30 23:10:22 +0530

    Update final.py

--- final.py
+++ final.py
@@ -8,10 +8,8 @@ import create_model as cm
 
 st.title("Automatic medical report generation from radiology chest x-ray using deep learning strategy")
 
-st.markdown("<small>by</small>Rajath R Pai",unsafe_allow_html=True)
-st.markdown("Sahasya D Rao")
-st.markdown("Shrilakshmi Pai N")
-st.markdown("Pratham Koodakandi")
+st.markdown("<small>by</small> Rajath R Pai",unsafe_allow_html=True)
+st.markdown("Sahasya D Rao\nShrilakshmi Pai N\nPratham Koodakandi")
 st.markdown("[<small>Github</small>](https://github.com/rajathpi/automatic-report-generator)",unsafe_allow_html=True)
 st.markdown("\nThis app will generate impression part of an X-ray report.\nYou can upload 2 X-rays that are front view and side view of chest of the same individual.")
 
